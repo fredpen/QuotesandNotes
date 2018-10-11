1e06af1be939efe1d06ddf8d1c0d67800cc5b746
Fred <fredrickksola@gmail.com>
2018-10-11 18:43:21 +0100

    succesfully created the error maodls which houses the feed back for users that try to log in without authorisation finally fix the issue as regards flwaless delivery on mobile devices

--- index.php
+++ index.php
@@ -1,78 +1,12 @@
 <?php
   require_once 'includes/header.php';
   require_once 'includes/indexLeftContainer.php';
-?>
+  require_once 'includes/error_modals.php';
 
-<!-- Modal for liking quotes-->
-<div class="modal fade bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	<div class="modal-dialog modal-sm" role="document">
-		<div class="modal-content">
-
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="card-title modal-title" id="myModalLabel"></h4>
-			</div>
-
-			<div class="modal-body text-center">
-            <div class="card-content">
-            	<h4 class="card-title">Only members can like a quote </h4>
-              <p class="card-description">
-                Sign up for free <a href="signIn.php"> here</a>
-              </p>
-              <p class="card-description">
-                already have an account? <a href="signIn.php"> Login</a>
-              </p>
-            </div>
-			</div>
-
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-			</div>
-
-		</div>
-	</div>
-</div>
-
-
-
-<!-- main section of the main container -->
-<div class="main-container">
-  <div class="frow">
-	<?php
-	if (!$mail) {?>
-		<div class="container">
-		  <div class="alert alert-info">
-			<div class="alert-icon">
-			  <i class="material-icons">info_outline</i>
-			</div>
-			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			  <span aria-hidden="true"><i class="material-icons">clear</i></span>
-			</button>
-			<b>Info alert:</b> Receipient mail column can't be left empty
-		  </div>
-		</div>
-	  <?php };?> 
-	 <?php
-
-	if ($mailSent) {?>
-			<div class="container">
-			 <div class="alert alert-success">
-			  <div class="alert-icon">
-				<i class="material-icons">check</i>
-			  </div>
-			  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				<span aria-hidden="true"><i class="material-icons">clear</i></span>
-			  </button>
-			  <b>Success:</b> quote has been sent to <?php echo $receipientMail; ?>
-			</div>
-		  </div>
-		<?php }; ?>
-		
-		<?php
 		while ($row = mysqli_fetch_array($quoteArray)) {
 		$quoteId = $row['id'];?>
 
-			<div class="col-sm-12">
+			<div class="quote-container">
 			  <div class="rotating-card-container manual-flip" style="width: 100% !important; height: 100% !important;">
 				<div class="card card-rotate">
 				  <div class="front" style="width: 100% !important; height: 100% !important;">
@@ -80,9 +14,9 @@
 					
 					<!-- the quote  -->
 					  <p class="card-title">
-						<?php echo $row['content']; ?>
+							<?php echo $row['content']; ?>
 					  </p>
-					
+	
 					<!-- the quote genre -->
 					  <p class="card-description">
 						<div class="genreList">
