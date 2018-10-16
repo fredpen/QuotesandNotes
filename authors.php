854efc0a5e6f79e68746b0aaeee7e2a8d1145bb0
Fred <fredrickksola@gmail.com>
2018-10-16 19:00:01 +0100

    added the navbar to hold the links to the author jump links

--- authors.php
+++ authors.php
@@ -16,9 +16,54 @@
 	<div class="container">
 	<div class="row">
 
+		 <nav class="navbar navbar-rose">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-icons">
+		                                <span class="sr-only">Toggle navigation</span>
+		                                <span class="icon-bar"></span>
+		                                <span class="icon-bar"></span>
+		                                <span class="icon-bar"></span>
+	                            	</button>
+                  <a class="navbar-brand" href="#pablo">Icons</a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="example-navbar-icons">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li>
+                      <a href="#pablo"><i class="material-icons">email</i></a>
+                    </li>
+                    <li>
+                      <a href="#pablo"><i class="material-icons">face</i></a>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#pablo" class="dropdown-toggle" data-toggle="dropdown">
+												<i class="material-icons">settings</i>
+												<b class="caret"></b>
+											</a>
+                      <ul class="dropdown-menu dropdown-menu-right">
+                        <li class="dropdown-header">Dropdown header</li>
+                        <li><a href="#pablo">Action</a></li>
+                        <li><a href="#pablo">Another action</a></li>
+                        <li><a href="#pablo">Something else here</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#pablo">Separated link</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#pablo">One more separated link</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+
+       
+
 		<div class="col-sm-12 text-center text-uppercase">
-			<a class="author_list label label-default" href="#a">a</a>
-			<a class="author_list" href="#b">b</a>
+			<a class="author_list label label-info" href="#a">a</a>
+			<a class="author_list label label-info" href="#b">b</a>
+			<a class="author_list label label-info" href="#c">c</a>
+			<!-- <a class="author_list" href="#b">b</a>
 			<a class="author_list" href="#c">c</a>
 			<a class="author_list" href="#d">d</a>
 			<a class="author_list" href="#e">e</a>
@@ -48,7 +93,7 @@
 			<a class="author_list" href="#t">t</a>
 			<a class="author_list" href="#t">t</a>
 			<a class="author_list" href="#t">t</a>
-			<a class="author_list" href="#t">t</a>
+			<a class="author_list" href="#t">t</a> -->
 		</div>
 		
 		
