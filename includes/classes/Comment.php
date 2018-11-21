7a0e10a03eefc90976bb9db6f940103a0393193f
Fred <fredrickksola@gmail.com>
2018-11-21 20:32:42 +0100

    creating the the html structure -  the bear and bone of the profile page

--- includes/classes/Comment.php
+++ includes/classes/Comment.php
@@ -38,32 +38,32 @@ class Comment
 
         // check if the comment is more than a year
         if ($dateInterval->format("%Y") > 0) {
-            $dtString = ($dateInterval->format("%Y") == 1 ? $dateInterval->format("%Y year ") : $dateInterval->format("%Y years "));
+            $dtString = ($dateInterval->format("%Y") == 1 ? $dateInterval->format("a year ") : $dateInterval->format("%Y years "));
             return $dtString;
         
         // check in relation to months
         } elseif ($dateInterval->format("%m") > 0) {
-            $dtString = ($dateInterval->format("%m") == 1 ? $dateInterval->format("%m month ") : $dateInterval->format("%m months "));
+            $dtString = ($dateInterval->format("%m") == 1 ? $dateInterval->format("a month ") : $dateInterval->format("%m months "));
             return $dtString;
 
         // check in relation to days
         } elseif ($dateInterval->format("%d") > 0) {
-            $dtString = ($dateInterval->format("%d") == 1 ? $dateInterval->format("%d day ") : $dateInterval->format("%d days "));
+            $dtString = ($dateInterval->format("%d") == 1 ? $dateInterval->format("a day ") : $dateInterval->format("%d days "));
             return $dtString;
 
         // check in relation to hours
         } elseif ($dateInterval->format("%h") > 0) {
-            $dtString = ($dateInterval->format("%h") == 1 ? $dateInterval->format("%h hour ") : $dateInterval->format("%h hours "));
+            $dtString = ($dateInterval->format("%h") == 1 ? $dateInterval->format("a hour") : $dateInterval->format("%h hours "));
             return $dtString;
 
         // check in relation to minutes
         } elseif ($dateInterval->format("%i") > 0) {
-            $dtString = ($dateInterval->format("%i") == 1 ? $dateInterval->format("%i minute ") : $dateInterval->format("%i minutes "));
+            $dtString = ($dateInterval->format("%i") == 1 ? $dateInterval->format("a minute ") : $dateInterval->format("%i minutes "));
             return $dtString;
 
         // check seconds
         } elseif ($dateInterval->format("%s") > 0) {
-            $dtString = ($dateInterval->format("%s") == 1 ? $dateInterval->format("%s second ") : $dateInterval->format("%s seconds "));
+            $dtString = ($dateInterval->format("%s") == 1 ? $dateInterval->format("a second ") : $dateInterval->format("%s seconds "));
             return $dtString;
         }
     }
--- profilePage.php
+++ profilePage.php
@@ -1,6 +1,8 @@
 <?php
+include_once 'includes/header.php';
+include_once 'includes/classes/Comment.php';
 
-require_once 'includes/header.php';
+$comment = new Comment($con);
 
 if (isset($_GET['id'])) {
     $userId = $_GET['id'];
@@ -8,17 +10,12 @@ if (isset($_GET['id'])) {
     header("Location: index.php");
 }
 
+$quoteDetails = $quote->fetchQuotesLovedByUser($userId);
+$userBiodata = $quote->fetchUserDetails($userId);
 //query all the user quotes interaction on the site
 $quoteArray = $quote->fetchQuotesLovedByUser($userId);
-
-
-$quoteDetails = $quote->fetchQuotesLovedByUser($userId);
-
 //query the total number of quote that are loved by the user
 $numOfQuoteLoveByUser = $quote->numberOfQuoteLoveByUser($userId);
-
-// query the user biodata submitted during registration from the database
-$userBiodata = $quote->fetchUserDetails($userId);
 
 // create an empty array for both the author and genre to push the vakues too fron the db
 $genreArray = array();
@@ -41,116 +38,234 @@ while ($row = mysqli_fetch_array($quoteDetails)) {
     }
 }
 ?>
+<div class="profile-page">
+<div class="page-header" data-parallax="true" style="background-image: url('assets/images/bg13.jpg');   background-position: center center;"></div>
 
-<div class="fcontainer">
-  <div class="frow">
-    <!-- left section of the main container -->
-    <div class="left-container topMargin65">
-      <!-- list of authors the user have interacted with  -->
-      <ul class="list-group">
-        <li class="list-group-item active">Authors you have shown interest in</li>
-        <!-- loop through authors that the user has liked  -->
-        <?php
-        foreach ($authorArray as $key => $value) {; ?>
-          <li class="list-group-item">
-            <a href="author.php?author=<?php echo $quote->authorId($value); ?>"><?php echo imagify($value); ?>
-         </a>
-      </li> <?php 
-        }; ?>
-</ul>
+	<div class="main main-raised">
+		<div class="profile-content">
+            <div class="container">
 
-<div class="topMargin65">
-   <ul class="list-group">
-     <li class="list-group-item active">Genres you have shown interest in</li>
-     <!-- loop through authors that the user has liked  -->
-    <?php
-    foreach ($genreArray as $key => $value) {; ?>
-       <li class="list-group-item">
-         <a href="author.php?author=<?php echo $quote->genreId($value); ?>"><?php echo $value; ?>
-          </a>
-       </li>
-    <?php 
-} ?>
-</ul>
-</div>
-</div> <!--left container-->
+                <div class="row">
+	                <div class="col-xs-6 col-xs-offset-3">
+        	           <div class="profile">
+	                        <div class="avatar">
+	                            <img src="assets/images/author/Barack_Obama.jpg" alt="Circle Image" class="img-circle img-responsive img-raised">
+	                        </div>
+	                        <div class="name">
+                                <h3 class="title"><?php echo $userBiodata['firstName'] . " " . $userBiodata['lastname']; ?> </h3>
+                                <span> - joined <?php echo $comment->dateInt($userBiodata['dt']); ?> ago </span>
+								<a href="#pablo" class="btn btn-just-icon btn-simple btn-dribbble"><i class="fa fa-dribbble"></i></a>
+                                <a href="#pablo" class="btn btn-just-icon btn-simple btn-twitter"><i class="fa fa-twitter"></i></a>
+                                <a href="#pablo" class="btn btn-just-icon btn-simple btn-pinterest"><i class="fa fa-pinterest"></i></a>
+	                        </div>
+	                    </div>
+    	            </div>
+                    <div class="col-xs-2 follow">
+	                   <button class="btn btn-fab btn-primary" rel="tooltip" title="Follow this user">
+                            <i class="material-icons">add</i>
+                        </button>
+	                </div>
+                </div>
 
 
-<div class="main-container topMargin65">
-  <div class="frow">
+                <div class="description text-center">
+                    <h6>Bio</h6>
+                    <p>An artist of considerable range, Chet Faker — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. </p>
+                </div>
 
-    <!-- the user bio datas -->
-    <div class="col-md-12">
-      <ul class="nav nav-pills nav-pills-rose">
-        <li class="active"><a href="#pill1" data-toggle="tab">Profile</a></li>
-        <li><a href="#pill2" data-toggle="tab">Details</a></li>
-
-        <li><a href="#pill3" data-toggle="tab">Social interactions</a></li>
-     </ul>
-     <div class="tab-content tab-space">
-        <div class="tab-pane active" id="pill1">
-          <?php
-// string conjured from the user bio data
-            $nameString = $userBiodata['lastname'] . " also known as " . $userBiodata['username'] . " joined us on the " . $userBiodata['dt'] .
-                "<br> He has liked " . $numOfQuoteLoveByUser . " quotes
-          <br> He has interacted with 3 authors and 7 genres of quotes";
-            ?>
-          <div class="card">
-            <div class="card-content">
-              <h4 class="text-cap card-title">
-                <?php echo $nameString; ?>
-             </h4>
-          </div>
-       </div>
-    </div>
-    <div class="tab-pane" id="pill2">
-      <?php
-// more details conjured from the user bio data
-        $profileString = "Full Name: " . $userBiodata['firstName'] . " " . $userBiodata['lastname'] .
-            "<br>    Email: " . $userBiodata['email'] . "<br>
-      Username: " . $userBiodata['username']; ?>
-      
-      <div class="card">
-       <div class="card-content">
-         <h4 class="card-title">
-           <?php echo $profileString; ?>
-        </h4>
-     </div>
-  </div>
-</div>
-<div class="tab-pane" id="pill3">
-  <?php
-// more details conjured from the user bio data
-    $socialString = $userBiodata['lastname'] . " has shared 20 quotes on facebook and 30 quotes on twitter";
-
-    ?>
-  <div class="card">
-    <div class="card-content">
-      <h4 class="card-title">
-        <?php echo $socialString; ?>
-     </h4>
-  </div>
-</div>
-</div>
-</div>
-</div>
-<!-- the main container that houses the quotes by the author  -->
-<div class="title col-sm-12 text-center text-grey">
-   <h3 class="title"> Quotes that you have liked </h3>
-</div>
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+						<div class="profile-tabs">
+		                    <div class="nav-align-center">
+								<ul class="nav nav-pills nav-pills-icons" role="tablist">
+									<li class="active">
+			                            <a href="#work" role="tab" data-toggle="tab">
+											<i class="material-icons">palette</i>
+											Work
+			                            </a>
+			                        </li>
+                                    <li>
+										<a href="#connections" role="tab" data-toggle="tab">
+											<i class="material-icons">people</i>
+											Connections
+										</a>
+									</li>
+			                        <li>
+			                            <a href="#media" role="tab" data-toggle="tab">
+											<i class="material-icons">camera</i>
+			                                Media
+			                            </a>
+			                        </li>
+			                    </ul>
 
 
-<div class="main-container">
-	<div class="masonry">
-        <?php require_once 'includes/indexMainContainer.php'; ?>
-    </div>
-</div>
+							</div>
+						</div>
+						<!-- End Profile Tabs -->
+					</div>
+                </div>
+
+                <div class="tab-content">
+
+                    <!-- the tab with the quotes collections -->
+			        <div class="tab-pane active work" id="work">
+				        <div class="row">
+	                        <div class="col-md-9">
+		                        <h4 class="title"><?php echo $userBiodata['lastname']; ?>'s Latest Collections</h4>
+		                        <!-- <div class="row collections"> -->
+                                    <div class="col-md-12">
+                                        <div class="profilePagemasonry"><?php include 'includes/indexMainContainer.php'; ?></div>
+			                        </div>
+                                <!-- </div> -->
+		                    </div>
+		                    <div class="col-md-2 col-md-offset-1 stats">
+			                    <h4 class="title">Interactions Stats</h4>
+			                    <ul class="list-unstyled">
+				                    <li><b><?php echo count($genreArray) ?></b> Genres</li>
+				                    <li><b><?php echo count($authorArray) ?></b> Authors</li>
+				                    <li><b><?php echo $numOfQuoteLoveByUser; ?></b> Likes</li>
+				                </ul>
+				                <hr />
+				                <h4 class="title">About his Work</h4>
+				                <p class="description">French luxury footwear and fashion. The footwear has incorporated shiny, red-lacquered soles that have become his signature.</p>
+				                <hr />
+				                <h4 class="title">Focus</h4>
+				                <span class="label label-primary">Footwear</span>
+				                <span class="label label-rose">Luxury</span>
+			                </div>
+	                    </div>
+                    </div>
+                    
+
+                    <div class="tab-pane connections" id="connections">
+                        <div class="row">
+            				<div class="col-md-5 col-md-offset-1">
+            					<div class="card card-profile card-plain">
+            						<div class="col-md-5">
+            							<div class="card-image">
+            								<a href="#pablo">
+            									<img class="img" src="../assets/img/faces/avatar.jpg" />
+            								</a>
+            							</div>
+            						</div>
+            						<div class="col-md-7">
+            							<div class="card-content">
+            								<h4 class="card-title">Gigi Hadid</h4>
+            								<h6 class="category text-muted">Model</h6>
+
+            								<p class="card-description">
+            									Don't be scared of the truth because we need to restart the human foundation in truth...
+            								</p>
+            							</div>
+            						</div>
+            					</div>
+            				</div>
+
+            				<div class="col-md-5">
+            					<div class="card card-profile card-plain">
+            						<div class="col-md-5">
+            							<div class="card-image">
+            								<a href="#pablo">
+            									<img class="img" src="../assets/img/faces/marc.jpg" />
+            								</a>
+            							</div>
+            						</div>
+            						<div class="col-md-7">
+            							<div class="card-content">
+            								<h4 class="card-title">Marc Jacobs</h4>
+            								<h6 class="category text-muted">Designer</h6>
+
+            								<p class="card-description">
+            									Don't be scared of the truth because we need to restart the human foundation in truth...
+            								</p>
+            							</div>
+            						</div>
+            					</div>
+            				</div>
+                        </div>
+                        <div class="row">
+            				<div class="col-md-5 col-md-offset-1">
+            					<div class="card card-profile card-plain">
+            						<div class="col-md-5">
+            							<div class="card-image">
+            								<a href="#pablo">
+            									<img class="img" src="../assets/img/faces/kendall.jpg" />
+            								</a>
+            							</div>
+            						</div>
+            						<div class="col-md-7">
+            							<div class="card-content">
+            								<h4 class="card-title">Kendall Jenner</h4>
+            								<h6 class="category text-muted">Model</h6>
+
+            								<p class="card-description">
+            									I love you like Kanye loves Kanye. Don't be scared of the truth.
+            								</p>
+            							</div>
+            						</div>
+            					</div>
+            				</div>
+
+            				<div class="col-md-5">
+            					<div class="card card-profile card-plain">
+            						<div class="col-md-5">
+            							<div class="card-image">
+            								<a href="#pablo">
+            									<img class="img" src="../assets/img/faces/card-profile2-square.jpg" />
+            								</a>
+            							</div>
+            						</div>
+            						<div class="col-md-7">
+            							<div class="card-content">
+            								<h4 class="card-title">George West</h4>
+            								<h6 class="category text-muted">Model/DJ</h6>
+
+            								<p class="card-description">
+            									I love you like Kanye loves Kanye.
+            								</p>
+            							</div>
+            						</div>
+            					</div>
+            				</div>
+
+            			</div>
+                    </div>
 
 
-</div>
-</div>
+                    <div class="tab-pane text-center gallery" id="media">
+						<div class="row">
+							<div class="col-md-3 col-md-offset-3">
+								<img src="../assets/img/examples/chris4.jpg" class="img-rounded" />
+								<img src="../assets/img/examples/chris6.jpg" class="img-rounded" />
+							</div>
+							<div class="col-md-3">
+								<img src="../assets/img/examples/chris7.jpg" class="img-rounded" />
+								<img src="../assets/img/examples/chris5.jpg" class="img-rounded" />
+								<img src="../assets/img/examples/chris9.jpg" class="img-rounded" />
+							</div>
+						</div>
+                    </div>
+
+
+			    </div>
+            </div>
+        </div>
+	</div>
+	</div>
 
 
 
-<?php require_once "includes/indexRightContainer.php";
-require_once 'includes/footer.php'; ?>
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+require_once 'includes/footer.php';
+?>
