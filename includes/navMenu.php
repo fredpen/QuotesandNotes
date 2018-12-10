43cb68551dadf38d4477217af46b4e2e8b852000
Oladipupo O. Fredrick <31975118+fredpen@users.noreply.github.com>
2018-12-10 07:10:41 +0100

    Update navMenu.php
    configure the google analytics script

--- includes/navMenu.php
+++ includes/navMenu.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html lang="en">
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130716173-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-130716173-1');
+        </script>
+
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <link rel="icon" type="image/png" href="assets/images/logo.png">
